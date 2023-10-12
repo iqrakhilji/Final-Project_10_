9870957733e476120971e5f58c8a45831d185dd6
iqrakhilji <143201499+iqrakhilji@users.noreply.github.com>
2023-10-12 20:06:39 +0530

    Final project

---
+++ 8_orderHistory.py
@@ -0,0 +1,123 @@
+[
+    {
+        "name": "Saleha",
+        "history": [
+            {
+                "name": "Chicken khema pizza",
+                "foodid": 2,
+                "quantity": 1,
+                "price": 320.0,
+                "discount": 3.5,
+                "stock": 30
+            },
+            {
+                "name": "Truffle Cake",
+                "foodid": 3,
+                "quantity": 500,
+                "price": 900.0,
+                "discount": 5.0,
+                "stock": 20
+            }
+        ]
+    },
+    {
+        "name": "Bobby",
+        "history": [
+            {
+                "name": "Chicken khema pizza",
+                "foodid": 2,
+                "quantity": 1,
+                "price": 320.0,
+                "discount": 3.5,
+                "stock": 30
+            },
+            {
+                "name": "Truffle Cake",
+                "foodid": 3,
+                "quantity": 500,
+                "price": 900.0,
+                "discount": 5.0,
+                "stock": 20
+            }
+        ]
+    },
+    {
+        "name": "Cindrella",
+        "history": [
+            {
+                "name": "Chicken khema pizza",
+                "foodid": 2,
+                "quantity": 1,
+                "price": 320.0,
+                "discount": 3.5,
+                "stock": 30
+            },
+            {
+                "name": "Truffle Cake",
+                "foodid": 3,
+                "quantity": 500,
+                "price": 900.0,
+                "discount": 5.0,
+                "stock": 20
+            }
+        ]
+    },
+    {
+        "name": "Uzma",
+        "history": [
+            {
+                "name": "Chicken khema pizza",
+                "foodid": 2,
+                "quantity": 1,
+                "price": 320.0,
+                "discount": 3.5,
+                "stock": 30
+            },
+            {
+                "name": "Truffle Cake",
+                "foodid": 3,
+                "quantity": 500,
+                "price": 900.0,
+                "discount": 5.0,
+                "stock": 20
+            }
+        ]
+    },
+    {
+        "name": "Elisha",
+        "history": [
+            {
+                "name": "Chicken khema pizza",
+                "foodid": 2,
+                "quantity": 1,
+                "price": 320.0,
+                "discount": 3.5,
+                "stock": 30
+            },
+            {
+                "name": "Truffle Cake",
+                "foodid": 3,
+                "quantity": 500,
+                "price": 900.0,
+                "discount": 5.0,
+                "stock": 20
+            },
+            {
+                "name": "Chicken khema pizza",
+                "foodid": 2,
+                "quantity": 1,
+                "price": 320.0,
+                "discount": 3.5,
+                "stock": 30
+            },
+            {
+                "name": "chiken Tikka",
+                "foodid": 576,
+                "quantity": "500g",
+                "price": 250,
+                "discount": 10.0,
+                "stock": 100
+            }
+        ]
+    }
+]
